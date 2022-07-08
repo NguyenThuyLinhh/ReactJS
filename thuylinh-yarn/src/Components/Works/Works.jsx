504f1e09b6f1c2e4a08efa0124bd56e856e5b352
TLinh <nguyenthuylinh08042001>
2022-07-08 21:31:23 +0700

    update day 8.7.22

--- thuylinh-yarn/src/Components/Works/Works.jsx
+++ thuylinh-yarn/src/Components/Works/Works.jsx
@@ -3,11 +3,12 @@ import "./Works.css";
 import Facebook from "../../Images/facebook.png";
 import Amazon from "../../Images/amazon.png";
 import Fiverr from "../../Images/fiverr.png";
-import Shopyfy from "../../Images/shopyfy.jpg";
+import Shopyfy from "../../Images/shopyfy.png";
 import Upwork from "../../Images/upwork.png";
 const Works = () => {
   return (
     <div className="works">
+      {/* left side */}
       <div className="awesome">
         <span>Works for All these</span>
         <span>Brands & Clients</span>
@@ -26,6 +27,29 @@ const Works = () => {
         <button className="button s-button">Here me</button>
 
         <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right side */}
+      <div className="w-right">
+        <div className="w-mainCircle">
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiverr} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Shopyfy} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </div>
+        {/* background Circle */}
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
       </div>
     </div>
   );
